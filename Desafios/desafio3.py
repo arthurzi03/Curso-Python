e8b2fce917f3dd2e1982b8cafb78b497fbf73879
Arthur Oliveira <arthur272411@gmail.com>
2024-09-20 19:11:12 -0300

    Atualizar

---
+++ Desafios/desafio3.py
@@ -0,0 +1,6 @@
+x = input("Calculadora de adição entre dois numeros: Digite o primeiro numero")
+y = input("Agora digite o segundo numero")
+x = int(x)
+y = int(y)
+soma = x + y
+print("seu numero final é", soma)
