66d9166b6e9e93c0a366722b5108df8730715c83
Arthur Oliveira <arthur272411@gmail.com>
2025-05-27 11:44:07 -0300

    Atualizar

--- AluraPython/elife.py
+++ AluraPython/elife.py
@@ -7,4 +7,10 @@ elif numero < 0:
     print("O número é negativo.")
 else:
     print("O número é zero.")
+
 # Verifica se um número é par ou ímpar
+numero = int(input("Digite um número: "))
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
